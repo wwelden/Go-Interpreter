1f68dd083d4ea2c1e4d3d496cde5125057a871f8
wwelden <wwelden@trinity.edu>
2024-11-30 17:11:35 -0500

    3.1

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -2,13 +2,31 @@ package repl
 
 import (
 	"Go_Interpreter/monkey/lexer"
-	"Go_Interpreter/monkey/token"
+	"Go_Interpreter/monkey/parser"
 	"bufio"
 	"fmt"
 	"io"
 )
 
 const PROMPT = ">> "
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+       '._ '-=-' _.'
+          '-----'
+`
+
+Excerpt From
+Writing An Interpreter In Go
+Thorsten Ball
+This material may be protected by copyright.
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,9 +40,23 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+    io.WriteString(out, MONKEY_FACE)
+    io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+    io.WriteString(out, " parser errors:\n")
+    for _, msg := range errors {
+        io.WriteString(out, "\t"+msg+"\n")
+    }
 }
