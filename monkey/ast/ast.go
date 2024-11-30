c2b6e37a0c1340da0a66c7b37cb8d4a5f45e9923
wwelden <wwelden@trinity.edu>
2024-11-29 22:39:33 -0500

    update

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -170,3 +170,45 @@ type Boolean struct {
 func (b *Boolean) expressionNode() 	{}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolena) String() string 	{ return b.Token.Literal }
+
+
+type IfExpression struct {
+	Token		token.Token
+	Condition	Expression
+	Consequence	*BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode()	{}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal}
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+type BlockStatment struct {
+	Token		token.Token
+	Statements	[]Statement
+}
+
+func (bs *BlockStatment) statementNode()	{}
+func (bs *BlockStatment) TokenLiteral() string { return bs.Token.Literal}
+func (bs *BlockStatment) String() string {
+	var out bytes.Buffer
+
+	for _, s := bs.Statements {
+		out.WriteString(s.String())
+	}
+	retrun out.String()
+}
