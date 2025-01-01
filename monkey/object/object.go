1009547a5cf850987413da46c5c62e233973fddb
wwelden <wwelden@trinity.edu>
2025-01-01 12:12:07 -0500

    go mod update and objects

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -7,7 +7,10 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEN_OBJ  = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
+
+type Null struct{}
 
 type Object interface {
 	Type() ObjectType
@@ -21,10 +24,11 @@ type Boolean struct {
 	Value bool
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
-
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() ObjectType { return BOOLEN_OBJ }
 
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (n *Null) Inspect() string  { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
