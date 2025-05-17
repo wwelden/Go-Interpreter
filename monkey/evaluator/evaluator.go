571f0ebd6aaf95491cf0c7bef70cf0f1e1a1843e
wwelden <wwelden@trinity.edu>
2025-05-17 01:00:51 -0500

    up to ch 3.6

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -24,6 +24,11 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
 
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 
@@ -53,6 +58,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -68,5 +75,55 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(
+    operator string,
+    left, right object.Object,
+) object.Object {
+    switch {
+    case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+        return evalIntegerInfixExpression(operator, left, right)
+		case operator == "==":
+				return nativeBoolToBooleanObject(left == right)
+		case operator == "!=":
+				return nativeBoolToBooleanObject(left != right)
+    default:
+        return NULL
+    }
+}
+
+func evalIntegerInfixExpression( operator string, left, right object.Object,) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+			return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+			return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+			return &object.Integer{Value: leftVal + rightVal}
+	case "/":
+			return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+			return &object.Integer{Value: leftVal < rightVal}
+	case ">":
+			return &object.Integer{Value: leftVal > rightVal}
+	case "==":
+			return &object.Integer{Value: leftVal == rightVal}
+	case "!=":
+			return &object.Integer{Value: leftVal != rightVal}
+	default:
+			return NULL
 	}
 }
